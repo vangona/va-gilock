536a10df6c976200a39e20dde201e07cc920eae0
vangona <rlarhksrud12@naver.com>
2022-05-02 23:09:01 +0900

    Fix: viewHeight to innerHeight

--- src/js/getScreenSize.js
+++ src/js/getScreenSize.js
@@ -12,9 +12,14 @@ function setScreenSize() {
       document.documentElement.clientWidth || 0,
       window.innerWidth || 0
     ) / 100;
+
   screenSize.vh =
     Math.max(
       document.documentElement.clientHeight || 0,
       window.innerHeight || 0
     ) / 100;
+
+  document.documentElement.style.setProperty("--vw", `${screenSize.vw}px`);
+
+  document.documentElement.style.setProperty("--vh", `${screenSize.vh}px`);
 }
